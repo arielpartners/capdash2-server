075e95f4417f1dd53123a631ad8b9423bce0e35a
Nicholas Iorio <niorio1@gmail.com>
2017-05-15 09:42:45 -0400

    WIP

--- app/services/utilization_service.rb
+++ app/services/utilization_service.rb
@@ -34,6 +34,52 @@ class UtilizationService
   end
 
   def self.averages_by_case_type
-    #TODO
+    ql = <<-SQL
+      SELECT case_type.name as group,
+      round(sum(c.avg_utilization),0) as average_utilization
+      -- count(places) as capacity
+      FROM (
+        SELECT shelter_building_id,
+        avg(count) as avg_utilization
+        FROM censuses
+        GROUP BY shelter_building_id
+      ) AS c
+      JOIN shelter_buildings
+      ON c.shelter_building_id = shelter_buildings.id
+      JOIN classifiers as case_type
+      ON shelter_buildings.case_type_id = case_type.id
+      GROUP BY case_type.name
+    SQL
+    results = ActiveRecord::Base.connection.exec_query(sql)
+    results = results.to_hash
+    byebug
+    results.each do |result|
+      capacity = CaseType.find_by(name: result['group']).places.count
+      capacity = result['capacity']
+      result[:average_capacity] = capacity
+      result[:percentage] = ((result['average_utilization'].to_f / capacity) * 100).round
+    end
+    byebug
+
+    # utilization_sum = ActiveRecord::Base.connection.exec_query <<-SQL
+    #   SELECT round(sum(c.avg_utilization),0) as total
+    #   FROM (
+    #     SELECT shelter_building_id,
+    #     avg(count) as avg_utilization
+    #     FROM censuses
+    #     GROUP BY shelter_building_id
+    #   ) AS c
+    # SQL
+    # total_utilization = utilization_sum[0]['total']
+    total_utilization = results.reduce(0){|sum, result| sum + result['average_utilization'].to_i}
+    total_capacity = Place.count
+    percentage = ((total_utilization.to_f / total_capacity) * 100).round
+    results << {
+      group: 'Total',
+      average_capacity: Place.count,
+      average_utilization: total_utilization,
+      percentage: percentage
+    }
+    results
   end
 end
