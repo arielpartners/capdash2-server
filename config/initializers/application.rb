285598853ef9be769490e9409ff85157a22ef5c7
Craeg Strong <cstrong@arielpartners.com>
2017-05-26 17:44:28 -0400

    rename postman login endpoint to login and up version to 0.0.2

--- config/initializers/application.rb
+++ config/initializers/application.rb
@@ -1,7 +1,7 @@
 module Cdserver
   class Application
     NAME = 'capdash2'.freeze
-    VERSION = '0.0.1'.freeze
+    VERSION = '0.0.2'.freeze
     DESCRIPTION = 'Shelter Capacity Dashboard'.freeze
   end
 end
