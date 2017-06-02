05d0a458fdc2a2368a7e664dda9fc49d93a1c8a9
Nicholas Iorio <niorio1@gmail.com>
2017-06-02 10:40:14 -0400

    default to json format

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # get 'healthcheck' see middleware_healthcheck.rb
-  scope :api do
+  scope :api, defaults: { format: :json } do
     get 'user', to: 'users#current'
     get 'info', to: 'diagnostics#info'
     post 'user_token' => 'user_token#create'
