3e35c697edbfdacc03a5768cba92a0d37babc827
Nicholas Iorio <niorio1@gmail.com>
2017-06-08 12:16:25 -0400

    refactor everything into ShelterUtilization

--- app/controllers/utilization_controller.rb
+++ app/controllers/utilization_controller.rb
@@ -3,9 +3,15 @@
 #
 class UtilizationController < ApplicationController
   def show
-    utilization = Utilization.new(utilization_params)
-    results = utilization.calculate
-    render json: results
+    if params[:group_by] == 'shelter'
+      results = ShelterUtilization.for_all_buildings
+      render json: results
+    elsif params[:group_by] == 'case-type'
+      results = ShelterUtilization.for_all_case_types
+      render json: results
+    else
+      render json: { error: 'Invalid group_by parameter' }, status: 400
+    end
   end
 
   private
--- app/services/shelter_utilization.rb
+++ app/services/shelter_utilization.rb
@@ -3,21 +3,60 @@
 #
 class ShelterUtilization
   attr_accessor :facility, :building, :address, :average_utilization,
-                :percentage, :capacity
+                :percentage, :capacity, :group
 
-  def initialize(shelter_building)
+  def self.for_all_buildings
+    ShelterBuilding.all.map do |sb|
+      new.for_building(sb)
+    end
+  end
+
+  def self.for_all_case_types
+    group_results = CaseType.all.map do |ct|
+      new.for_group(ct)
+    end
+    group_results << totals
+  end
+
+  def self.totals
+    totals = ShelterUtilization.new
+    totals.group = 'Total'
+    totals.capacity = Place.count
+    avgs = Census.group(:shelter_building_id).average(:count)
+    total_avg_utilization = avgs.values.reduce(:+)
+    totals.calculate_percentage(total_avg_utilization)
+    totals
+  end
+
+  def initialize
+    self.average_utilization = nil
+    self.percentage = nil
+  end
+
+  def for_building(shelter_building)
+    self.capacity = shelter_building.places.count
     self.facility = shelter_building.shelter.name
     self.building = shelter_building.name
     self.address = shelter_building.address.line1
-    self.average_utilization = nil
-    self.percentage = nil
-    self.capacity = shelter_building.places.count
+    utilization = Census.where(shelter_building: shelter_building)
+                        .average(:count)
+    calculate_percentage(utilization)
+    self
+  end
+
+  def for_group(group)
+    self.group = group.name
+    self.capacity = group.shelter_buildings.joins(:places).count(:places)
+    utilization = Census.includes(:shelter_building)
+                        .where('shelter_buildings.case_type_slug = ?',
+                               group.slug).average(:count)
+    calculate_percentage(utilization)
+    self
   end
 
   def calculate_percentage(utilization)
-    return self unless utilization
+    return unless utilization
     self.average_utilization = utilization.round
     self.percentage = ((utilization / capacity) * 100).round
-    self
   end
 end
